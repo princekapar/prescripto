fd302f56b66fac15108f2adf07b27ba69f321efe
princekapar <princekapar02@gmail.com>
2026-07-02 22:19:46 +0530

    Fix user registration issue

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
 
   const {backendUrl,token,setToken} = useContext(AppContext)
 
-  const [state, setState] = useState('Sign up')
+  const [state, setState] = useState('Sign Up')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [name, setName] = useState('')
@@ -62,10 +62,10 @@ const Login = () => {
     <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center'>
 
       <div className='flex flex-col gap-3 border-none m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg'>
-        <p className='text-2xl font-semibold'>{state === 'Sign up' ? 'Create Account' : 'Login'}</p>
-        <p>Please {state === 'Sign up' ? 'sign up' : 'log in'} to book an appointment</p>
+        <p className='text-2xl font-semibold'>{state === 'Sign Up' ? 'Create Account' : 'Login'}</p>
+        <p>Please {state === 'Sign Up' ? 'sign up' : 'log in'} to book an appointment</p>
         {
-          state === 'Sign up' &&  <div className='w-full '>
+          state === 'Sign Up' &&  <div className='w-full '>
           <p>Full Name</p>
           <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="text" onChange={(e)=>setName(e.target.value)} value={name}/>
         </div>
@@ -80,12 +80,12 @@ const Login = () => {
           <p>Password</p>
           <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="password" onChange={(e)=>setPassword(e.target.value)} value={password}/>
         </div>
-        <button type='submit' className='bg-primary-blue text-white w-full py-2 rounded-md text-base' type="submit">{state === 'Sign up' ? 'Create Account' : 'Login'}</button>
+        <button type='submit' className='bg-primary-blue text-white w-full py-2 rounded-md text-base' type="submit">{state === 'Sign Up' ? 'Create Account' : 'Login'}</button>
 
         {
-          state === 'Sign up' ?
+          state === 'Sign Up' ?
             <p>Already have an anccount? <span onClick={()=>setState('Login')} className='text-primary-blue underline cursor-pointer'>Login here</span></p>
-            : <p>Create an new account? <span onClick={()=>setState('Sign up')} className='text-primary-blue underline cursor-pointer'>Click here</span></p>
+            : <p>Create an new account? <span onClick={()=>setState('Sign Up')} className='text-primary-blue underline cursor-pointer'>Click here</span></p>
         }
       </div>
       
